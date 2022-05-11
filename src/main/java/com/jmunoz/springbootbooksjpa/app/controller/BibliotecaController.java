fff471009943a404d9a746be7d471f65cc2ac248
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-11 20:31:02 +0200

    Test al Controller. Test getUsuarioHttpRequest

--- src/main/java/com/jmunoz/springbootbooksjpa/app/controller/BibliotecaController.java
+++ src/main/java/com/jmunoz/springbootbooksjpa/app/controller/BibliotecaController.java
@@ -1,6 +1,8 @@
 package com.jmunoz.springbootbooksjpa.app.controller;
 
 import com.jmunoz.springbootbooksjpa.app.models.Biblioteca;
+import com.jmunoz.springbootbooksjpa.app.models.UsuarioComprador;
+import com.jmunoz.springbootbooksjpa.app.service.UsuarioYLibroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +15,14 @@ public class BibliotecaController {
     @Autowired
     private Biblioteca biblioteca;
 
+    @Autowired
+    private UsuarioYLibroService usuarioService;
+
     @GetMapping("/")
     public String getUsuarios(Model m) {
+        Iterable<UsuarioComprador> usuariosCompradores = usuarioService.getBiblioteca();
+        m.addAttribute("usuarios", usuariosCompradores);
+
         return "index";
     }
 
